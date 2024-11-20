3540a33db4686dd5e6c85ea42bf4f89f95399a3d
AyKrimino <ayoub.krimi14@gmail.com>
2024-11-20 02:15:28 +0100

    feat(services): add auth services and axios instance

--- frontend/src/services/api.js
+++
@@ -1,1 +0,0 @@
-// Base Axios instance
--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,1 +1,63 @@
-// Auth API calls
+import axiosInstance from "./axiosInstance";
+
+/**
+ * Register a new user.
+ * @param {Object} userDetails - The user details for registration.
+ * @returns {Promise<Object>} - The server response.
+ */
+export const register = async (userDetails) => {
+  try {
+    const response = await axiosInstance.post("/register", userDetails);
+    if (response.status === 201) return response.data;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+/**
+ * Login a user.
+ * @param {Object} credentials - The user credentials.
+ * @returns {Promise<Object>} - The server response containing tokens.
+ */
+export const login = async (credentials) => {
+  try {
+    const response = await axiosInstance.post("/login", credentials);
+    if (response.status === 200) return response.data;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+/**
+ * Logout the current user.
+ * @param {Object} credentials - The user credentials.
+ * @returns {Promise<void>}
+ */
+export const logout = async (credentials) => {
+  try {
+    const response = await axiosInstance.post("/logout", credentials);
+    if (response.status === 205) return;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+/**
+ * Refresh the access token.
+ * @param {string} refreshToken - The refresh token.
+ * @returns {Promise<Object>} - The new access token.
+ */
+export const refresh = async (refreshToken) => {
+  try {
+    const response = await axiosInstance.post("/token/refresh", {
+      refresh: refreshToken,
+    });
+    if (response.status === 200) return response.data;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
