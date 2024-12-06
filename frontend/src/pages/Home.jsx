82e5ba8f1d50c29bc3aed05c3889a6302eba6720
AyKrimino <ayoub.krimi14@gmail.com>
2024-12-06 23:50:30 +0100

    feat: created see all classrooms button on home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,35 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import AuthContext from "../context/AuthContext";
 import WelcomeBack from "../components/WelcomBack";
 import ListClassroomCards from "../features/classrooms/ListClassroomCards";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { getClassroomList } from "../services/Classrooms";
+import { getStudentsClassroomsList } from "../services/studentsClassrooms";
 
 const Home = () => {
   const { auth } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [hasClassrooms, setHasClassrooms] = useState(false);
+
+  useEffect(() => {
+    const fetchClassrooms = async () => {
+      try {
+        const response = 
+          auth.role === "teacher" 
+            ? await getClassroomList() 
+            : await getStudentsClassroomsList();
+
+        setHasClassrooms(
+          auth.role === "teacher" ? response.data.length > 0 : response.data.some(obj => obj.classroom)
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchClassrooms();
+  }, [auth.role]);
 
   if (!auth) return <p>Loading...</p>;
 
@@ -12,8 +37,20 @@ const Home = () => {
     <div className="h-[100%] w-[100%] overflow-y-auto overflow-x-hidden bg-gray-100">
       <WelcomeBack />
       <ListClassroomCards limit={8} />
+      {hasClassrooms && (
+        <div className="flex justify-center mt-2 mb-4">
+          <Button 
+            variant="contained" 
+            color="primary" 
+            onClick={() => navigate("/classrooms")}
+          >
+            See All Classrooms
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
 
 export default Home;
+
