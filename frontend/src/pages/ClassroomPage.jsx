572e862d16a44cf63ca4f7546c23871db536532d
AyKrimino <ayoub.krimi14@gmail.com>
2024-12-07 12:01:10 +0100

    feat: added classroom meta data to the classroom page

--- frontend/src/pages/ClassroomPage.jsx
+++ frontend/src/pages/ClassroomPage.jsx
@@ -1,13 +1,33 @@
-import { useState, useContext } from "react";
-import { Tabs, Tab, Box, Typography, Button } from "@mui/material";
+import { useContext, useEffect, useState } from "react";
+import { Box, Tab, Tabs, Typography } from "@mui/material";
 import PostsTab from "../features/posts/PostsTab";
 import QuizzesTab from "../features/quiz/QuizzesTab";
 import AuthContext from "../context/AuthContext";
 import Breadcrumb from "../components/BreadcrumbNav";
+import { useParams } from "react-router-dom";
+import { getClassroomById } from "../services/Classrooms";
 
-const ClassroomPage = ({ classroomId }) => {
+const ClassroomPage = () => {
   const { auth } = useContext(AuthContext);
   const [activeTab, setActiveTab] = useState(0);
+  const [classroomName, setClassroomName] = useState("");
+  const [teacherName, setTeacherName] = useState("");
+  const { classroomId } = useParams();
+
+  useEffect(() => {
+    const fetchClassroomName = async () => {
+      try {
+        const response = await getClassroomById(classroomId);
+        setClassroomName(response.data.name);
+        setTeacherName(
+          `${response.data.teacher.user_first_name} ${response.data.teacher.user_last_name}`,
+        );
+      } catch (error) {
+        console.error("Error fetching classroom name:", error);
+      }
+    };
+    fetchClassroomName();
+  }, [classroomId]);
 
   const handleTabChange = (event, newValue) => {
     setActiveTab(newValue);
@@ -17,8 +37,14 @@ const ClassroomPage = ({ classroomId }) => {
     <div className="h-[100%] w-[100%] bg-gray-100 overflow-y-auto">
       <Breadcrumb currentPage="Classroom" />
       <Box sx={{ padding: "16px", display: "flex", flexDirection: "column" }}>
-        <Typography variant="h4" mb={2}>
-          Classroom Details
+        <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+          Classroom: {classroomName}
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          sx={{ color: "text.secondary", fontStyle: "italic" }}
+        >
+          Instructor: {teacherName}
         </Typography>
         <Tabs
           value={activeTab}
